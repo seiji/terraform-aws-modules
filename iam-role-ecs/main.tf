fbcf95e0fff4bea04d928eaa126e37912197b796
seiji <toyama.seiji@gmail.com>
2020-02-26 19:58:42 +0900

    Fix iam-role-xx

--- iam-role-ecs/main.tf
+++ iam-role-ecs/main.tf
@@ -2,7 +2,7 @@ module iam_role_ecs_task_execution {
   source     = "../iam-role"
   name       = var.iam_role_name_ecs_task_execution
   identifier = "ecs-tasks.amazonaws.com"
-  policiy_json_list = [
+  policy_json_list = [
     data.aws_iam_policy_document.ecs_task_execution.json,
     data.aws_iam_policy_document.ssm_read_only.json,
     data.aws_iam_policy_document.cwa_server.json,
@@ -13,7 +13,7 @@ module iam_role_ecs_service {
   source     = "../iam-role"
   name       = var.iam_role_name_ecs_service
   identifier = "ecs.amazonaws.com"
-  policiy_json_list = [
+  policy_json_list = [
     data.aws_iam_policy_document.ec2_container.json,
   ]
 }
@@ -22,7 +22,7 @@ module iam_role_ecs_instance {
   source     = "../iam-role"
   name       = var.iam_role_name_ecs_instance
   identifier = "ec2.amazonaws.com"
-  policiy_json_list = [
+  policy_json_list = [
     data.aws_iam_policy_document.ec2_container_ec2.json,
     data.aws_iam_policy_document.ec2_for_ssm.json,
   ]
--- iam-role-es-cognito/main.tf
+++ iam-role-es-cognito/main.tf
@@ -2,7 +2,7 @@ module iam_role_es_cognito_access {
   source     = "../iam-role"
   name       = var.name
   identifier = "es.amazonaws.com"
-  policiy_json_list = [
+  policy_json_list = [
     data.aws_iam_policy_document.es_cognito_access.json,
   ]
 }

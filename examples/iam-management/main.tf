92f0d1d508a46380745d403ac52b50a188d487f2
seiji <toyama.seiji@gmail.com>
2020-03-02 18:32:21 +0900

    Fia iam-mgmt

--- examples/iam-management/main.tf
+++ examples/iam-management/main.tf
@@ -26,31 +26,23 @@ data aws_iam_policy iam_user_change_password {
   arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
 
-module group_users {
-  source = "../../iam-group"
-  name   = "users"
-  policies = [
-    data.aws_iam_policy.iam_user_change_password.arn,
-    module.iam_policy_custom.allow_access_key.arn,
-    module.iam_policy_custom.allow_mfa_device.arn,
-    module.iam_policy_custom.allow_ssm_session.arn,
-  ]
-}
-
 module group_admin {
   source = "../../iam-group"
   name   = "admin"
   policies = [
+    data.aws_iam_policy.iam_user_change_password.arn,
+    module.iam_policy_custom.allow_access_key.arn,
+    module.iam_policy_custom.allow_mfa_device.arn,
   ]
 }
-
-# module.iam_policy_managed.power_user_access.arn
-# module.iam_policy_managed.administrator_access.arn
 
 module group_developers {
   source = "../../iam-group"
   name   = "developers"
   policies = [
+    data.aws_iam_policy.iam_user_change_password.arn,
+    module.iam_policy_custom.allow_access_key.arn,
+    module.iam_policy_custom.allow_mfa_device.arn,
   ]
 }
 
@@ -58,18 +50,12 @@ module users {
   source = "../../iam-users"
   users = [
     {
-      name = "admin"
-      groups = [
-        module.group_users.name,
-        module.group_admin.name,
-      ]
+      name   = "admin"
+      groups = [module.group_admin.name]
     },
     {
-      name = "guest1"
-      groups = [
-        module.group_users.name,
-        module.group_developers.name,
-      ]
+      name   = "guest1"
+      groups = [module.group_developers.name]
     },
     {
       name   = "guest2"
@@ -89,12 +75,11 @@ data aws_iam_policy administrator_access {
 module assume_admin_role {
   source = "../../iam-role"
   name   = "assume-admin-role"
+  policy_arns = [
+    data.aws_iam_policy.administrator_access.arn,
+  ]
   principals = {
     type        = "AWS"
     identifiers = [module.users.users["admin"].arn]
   }
-  policy_arns = [
-    data.aws_iam_policy.administrator_access.arn,
-  ]
 }
-

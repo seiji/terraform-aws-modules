1e63650be25f13c54373cdf4eb1038eef9cb2b1e
seiji <toyama.seiji@gmail.com>
2020-03-07 13:05:32 +0900

    Fix iam-mgmt

---
+++ examples/iam-management/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12.0"
+  backend "s3" {
+    bucket         = "terraform-aws-modules-tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+    key            = "iam-management.examples"
+    dynamodb_table = "terraform-aws-modules-tfstate-lock"
+  }
+}
+
+provider "aws" {
+  version = ">= 2.50"
+  region  = "ap-northeast-1"
+}

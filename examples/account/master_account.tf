061a73e138df922d1fce051a60a9cad358891447
seiji <toyama.seiji@gmail.com>
2020-05-15 19:45:46 +0900

    Remove example of account

--- examples/account/master_account.tf
+++
@@ -1,9 +0,0 @@
-module iam_account {
-  source = "../../iam/account"
-  password_policy = {
-    hard_expiry               = false
-    max_password_age          = null
-    minimum_password_length   = 32
-    password_reuse_prevention = 24
-  }
-}

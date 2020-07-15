ab8a1043527e03a42d837fc91519468eabce3b9d
Seiji Toyama <toyama.seiji@gmail.com>
2020-07-15 23:39:20 +0900

    Update label module

--- label/main.tf
+++ label/main.tf
@@ -1,27 +1,24 @@
 locals {
-  label_order = ["namespace", "stage", "name", "attributes"]
-  delimiter   = var.delimiter
+  label_order = ["namespace", "stage", "attributes"]
+
   context = {
-    name       = var.name
     namespace  = var.namespace
     stage      = var.stage
-    attributes = join(local.delimiter, var.attributes)
+    attributes = join(var.delimiter, var.attributes)
   }
-  add_tags = var.add_tags
 
-  labels = [for l in local.label_order : local.context[l] if length(local.context[l]) > 0]
-  id     = lower(join(local.delimiter, local.labels))
+  labels      = [for l in local.label_order : local.context[l] if length(local.context[l]) > 0]
+  id          = lower(format("%s%s", var.prefix, join(var.delimiter, local.labels)))
+  name        = var.name == "" ? local.id : var.name
+  tag_context = merge(local.context, { name = local.name })
 
-  tag_context = merge(local.context, {
-    name = local.context.name == "" ? local.id : local.context.name
-  })
-  _tags = { for l in keys(local.tag_context) : title(l) => local.tag_context[l] if length(local.tag_context[l]) > 0 }
+  tag_title = merge({ for l in keys(local.tag_context) : title(l) => local.tag_context[l] if length(local.tag_context[l]) > 0 }, var.add_tags)
 
-  tags = merge(local._tags, local.add_tags)
+  tags = merge(local.tag_title, var.add_tags)
   tags_list = [
-    for key in keys(local._tags) : merge({
+    for key in keys(local.tag_title) : merge({
       key   = key
       value = local.tags[key]
-    }, local.add_tags)
+    }, var.add_tags)
   ]
 }

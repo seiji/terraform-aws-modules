f43b2a752d8929d09d60e514513541cae21af440
Seiji Toyama <toyama.seiji@gmail.com>
2021-03-05 11:27:53 +0900

    Rename cru-report

--- aws/codebuild/outputs.tf
+++ aws/codebuild/outputs.tf
@@ -1,3 +1,11 @@
+output "arn" {
+  value = aws_codebuild_project.this.arn
+}
+
+output "id" {
+  value = aws_codebuild_project.this.id
+}
+
 output "name" {
   value = aws_codebuild_project.this.name
 }
